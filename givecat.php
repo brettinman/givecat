1f138fd091f9a3d52d8505e904a7083e2f44074a
Brett Inman <binman@marinsoftware.com>
2015-01-10 01:11:29 -0800

    Comments and more user testing

--- givecat.php
+++ givecat.php
@@ -3,6 +3,7 @@
 // insert your client id for imgur api access
 $client_id = 'your_client_id';
 
+// build the api request
 $url = 'https://api.imgur.com/3/gallery/r/lolcats/';
 $headers = array("Authorization: Client-ID $client_id");
 
@@ -14,14 +15,13 @@ CURLOPT_HTTPHEADER => $headers,
 CURLOPT_RETURNTRANSFER => 1,
 ));
 
+// request, convert json response into array
 $json_returned = curl_exec($curl);
 curl_close ($curl);
-
-
 $data = json_decode($json_returned);
 $id = $data->data[array_rand($data->data)]->id;
 
-
+// respond with link to image chosen above
 header('Content-Type: application/json');
 die(json_encode(array(
 'text' => "http://i.imgur.com/$id.jpg"
